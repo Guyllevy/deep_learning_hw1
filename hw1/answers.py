b2531167e79555d58f734cd1959a080ac554774c
guy <guylleviii@gmail.com>
2023-04-03 19:18:10 +0300

    answering dry question 1

--- hw1/answers.py
+++ hw1/answers.py
@@ -12,9 +12,19 @@ part1_q1 = r"""
 **Your answer:**
 
 Statement 1 : "The test set allows us to estimate our in-sample error."
-This is False, the in-sample error is the error we get on the training set
+This statement is false, the in-sample error is the error we get on the examples from the training set.
+The test set allows us to estimate the out-sample error, the error we get on example we haven't seen while training the model.
+So we can get small error on the training set while the error on the test set is high, and so the test set is not a good estimator to the in-sample error.
 
-TESTING GIT TESTING GIT TESTING GIT
+Statement 2 : "Any split of the data into two disjoint subsets would constitute an equally useful train-test split."
+This statement false, for example a split which has 1% of the examples in the training set and the rest in the test set will probably won't do very well compared to another split of say 50-50. Thats because we learned very little from existing examples.
+Or another example: if we use a split in which there is small amount of one class samples compared to the other class samples, the split is imbalanced and therefore could lead to poor model performance.
+
+Statement 3 : "The test-set should not be used during cross-validation."
+This is true, the purpose of the test set is to evaluate the final model's ability to generelize. so if we use the test set during cross validation we learn from it which loses this purpose.
+
+Statement 4 : "After performing cross-validation, we use the validation-set performance of each fold as a proxy for the model's generalization error."
+This is true, after performing cross-validation, we use the average of the validation set performance across all folds to estimate the model's generalization error.
 
 """
 
@@ -22,12 +32,6 @@ part1_q2 = r"""
 **Your answer:**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
